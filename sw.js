f72270e57483d6d9522ecefe1fc26f5fb82ad516
Belgleiner <belgleiner@yahoo.com.br>
2026-03-28 13:22:52 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,25 @@
-const cacheName = 'bloco-notas-v1';
-const assets = ['./', './index.html'];
+const cacheName = 'notas-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icone.png'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+// Instala o Service Worker e guarda os arquivos no celular
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Permite que o app abra mesmo sem internet (Offline)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
